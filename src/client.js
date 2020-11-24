be19a177600ea3dd9300cf06d812781b3af54920
boramriganka <42547729+boramriganka@users.noreply.github.com>
2020-11-24 13:20:04 +0530

    Update client.js

--- src/client.js
+++ src/client.js
@@ -2,6 +2,6 @@ import * as contentful from 'contentful'
 
 
 export const client = contentful.createClient({
-    space: process.env.REACT_APP_SPACE_ID,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: 'ggcbrqkv3oyk',
+    accessToken: 'A7Wa1h7ph9iQrAHf2EYNkyHnw_w5YZOdMZ0Vl0jOgdo'
 })
